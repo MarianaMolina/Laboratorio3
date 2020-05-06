32f79b56416791061b8858054fcacca8b3950aa3
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-05-05 19:37:10 -0600

    laboratorio 3

---
+++ Laboratorio3/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,70 @@
+package com.example.demo.controller;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.Valid;
+import com.example.demo.domain.Student;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class MainController {
+	
+	private List<Student> students = new ArrayList<Student>();
+	
+	
+	@GetMapping(path ="/ejemplo1", produces = MediaType.TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public String ejemplo1() {	
+		return "Bienvenidos\n" + "Programacion N-Capas";
+	}
+	
+	
+	
+	@GetMapping("/ejemplo2")
+	public @ResponseBody List<Student> ejemplo2(){
+		return Arrays.asList(
+				new Student("Nombre1", "Apellido1", "10/10/1992", "carrera x", true),
+				new Student("Nombre2", "Apellido2", "10/10/1992", "carrera Y", false)
+				);
+	}
+	
+	@GetMapping ("/inicio")
+	public String inicio(Student student) {
+		return "index";
+	}
+	
+	
+	
+	@PostMapping("/formData")
+	public ModelAndView procesar(Student student) {
+		students.add(student);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		mav.addObject("student", new Student());
+		return mav;
+	}
+	
+	@GetMapping("/listado")
+	public ModelAndView listado() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("listado");
+		mav.addObject("studentList", this.students);
+		return mav;
+	}
+	
+	
+	
+
+	
+	
+}
